a89e9c483268a8989c01eaff8b64cf72b4c126bc
ishant321 <ishantnayakk@gmail.com>
2023-09-16 20:09:43 +0530

    static files are updated

--- index.js
+++ index.js
@@ -30,8 +30,9 @@ app.locals._ = _;
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static("public"));
-app.use(express.static("images"));
+app.use(express.static(__dirname + "/public"));
+app.use("/static", express.static(__dirname + "public"));
+app.use("/static", express.static(__dirname + "images"));
 app.use(flash());
 
 app.use(session({
